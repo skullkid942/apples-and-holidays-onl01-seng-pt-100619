87a3db514e59ce23a67bba5af015088ac244ee11
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-20 17:27:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,6 +44,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].push(supply)
 end
 
+add_supply_to_memorial_day(holiday_supplies, "Grill")
+
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
